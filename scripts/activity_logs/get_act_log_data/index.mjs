8c6d1ece319e7ce653528e7c348c7e7f0d1d71fb
TJ Helton <tjhelton99@gmail.com>
2025-01-10 10:59:56 -0600

    adding activity log script

--- scripts/activity_logs/get_act_log_data/index.mjs
+++ scripts/activity_logs/get_act_log_data/index.mjs
@@ -1,37 +1,61 @@
-import winston from 'winston';
+import dotenv from 'dotenv'
+import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
+dotenv.config() 
 
-const logger = winston.createLogger({
-  level:'info',
-  format: winston.format.simple(),
-  transports: [
-    new winston.transports.File({
-      filename: 'users_changes.log',
-      level: 'info'
-  }),
-  ],
-});
+//change activity types as needed
+const activityTypes = ['action.delete_action_label', 'action.create_action_label']
 
-const activityTypes = ['org.users_added','org.user_removed','org.users_deactivated','org.user_activated']
-
-const token = process.argv[2]
-const rootUrl = 'https://api.safetyculture.io'
+const outputCsvPath = 'output.csv'
+const token = process.env.TOKEN
+const url = 'https://api.safetyculture.io'
 const options = {
     method: 'GET',
-    headers: {accept: 'application/json', authorization: 'Bearer ' + token}
+    headers: {
+      accept: 'application/json',
+      authorization: `Bearer ${token}`}
   };
+
+//change csv structure as needed
+const csvWriter = createCsvWriter({
+  path: outputCsvPath,
+  header: [
+    { id: 'type', title: 'type' },
+    { id: 'eventTime', title: 'eventTime' },
+    { id: 'userId', title: 'userId' }
+  ],
+})
+
+async function writer(type,time,user){
+  const record = [
+    {
+      type: type,
+      eventTime: time,
+      userId: user
+    }
+  ]
+  await csvWriter.writeRecords(record)
+};
 
 async function getLogs() {
     let appendUrl = '/feed/activity_log_events'
-
+    let iterationLog = 0
     while(appendUrl !== null){
-        const response = await fetch(rootUrl+appendUrl,options)
-        const jsonData = await response.json()
-        for(const item of jsonData.data){
-            if(activityTypes.includes(item.type)){
-                logger.log('info', `${item.type}:${item.event_at}:${item.user_id}`)
-        };
-        appendUrl = jsonData.metadata.next_page
-    };
-}}
+        const response = await fetch(`${url}${appendUrl}`,options)
+        if(!response.ok){
+          console.log(`page ${iterationLog} fetch failed: ${url}${appendUrl}`)
+          iterationLog++
+        } else {
+          console.log(`page ${iterationLog} fetched!`)
+          iterationLog++
+          const jsonData = await response.json()
+          for(const item of jsonData.data){
+              if(activityTypes.includes(item.type)){
+                //change writer arguments to match csvWriter L18
+                  await writer(item.type, item.event_at, item.user_id)
+          }
+          appendUrl = jsonData.metadata.next_page
+      }
+        }
+}};
 
-getLogs()
+await getLogs()
---
+++ scripts/activity_logs/get_act_log_data/readme.md
@@ -0,0 +1,62 @@
+# Get Activity Log Data
+<!-- <script description> -->
+This script allows for iterating through the activity log for an envrionment, and returning a CSV summary of the events requested.
+
+## Set up:
+
+Ensure dependencies are installed by running the below command in the directory of the script:
+
+```bash
+npm i
+```
+
+Create a .env file with a `TOKEN` parameter as follows:
+
+```bash
+TOKEN=5b1d73376dhy2a92960a0171b...
+```
+
+This script will need to be set up depending on what data needs to be returned. Structure the CSV as needed by changing the `csvWriter` variable:
+
+```js
+const csvWriter = createCsvWriter({
+  path: outputCsvPath,
+  header: [
+    { id: 'type', title: 'type' },
+    { id: 'eventTime', title: 'eventTime' },
+    { id: 'userId', title: 'userId' }
+  ],
+})
+```
+
+Note that any changes made to `csvWriter` will need to be changed in the declartion and call of the `writer` function:
+
+```js
+async function writer(type,time,user){
+  const record = [
+    {
+      type: type,
+      eventTime: time,
+      userId: user
+    }
+  ]
+  await csvWriter.writeRecords(record)
+};
+```
+
+```js
+await writer(item.type, item.event_at, item.user_id)
+```
+
+## Running the script:
+
+Once the set up is complete, run the following command in a terminal:
+`node index.mjs`
+
+## Outputs:
+
+The script will output a CSV of the relevant activity log events.
+
+## Additional Comments
+
+Note that the activity log only returns data from the past 30 days. Any data needed beyond that time frame will not be able to be fetched via the SafetyCulture API.
